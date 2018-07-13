405f5804a778e573908fa56efe4b07765785d861
Diogo Lundberg <dclundberg@gmail.com>
2018-07-13 19:35:15 -0300

    Add Processing

--- app/controllers/messages.py
+++ app/controllers/messages.py
@@ -29,4 +29,6 @@ def index():
         key = record['s3']['object']['key']
         file_name = re.sub(r'^[^/]+/', '', key)
         download(key, file_name)
+        process(file_name)
+        upload(file_name)
     return 'ok', 200
