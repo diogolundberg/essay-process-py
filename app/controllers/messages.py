bbdb4a179e3c7db2a6e4990f56d40f896a816e04
Diogo Lundberg <dclundberg@gmail.com>
2018-07-13 11:53:43 -0300

    Logger

--- app/controllers/messages.py
+++ app/controllers/messages.py
@@ -1,5 +1,21 @@
 from app.services import download, process, upload, delete
 from flask import Blueprint, request
+import logging
+import logging.handlers
+
+
+from os import getcwd, path
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+LOG_FILE = path.join(getcwd(), 'requests.log')
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 blueprint = Blueprint('messages', __name__, url_prefix='/messages')
 
@@ -8,6 +24,7 @@ def index():
     records = request.json.get('Records')
     if not records: return 'ok', 200
     for record in records:
+        logger.info(record)
         key = record['s3']['object']['key']
         download(key)
         process(key)
